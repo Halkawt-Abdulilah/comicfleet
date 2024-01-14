dfb242416e63bece7a81154aa66031f4636c5515
Halkawt Abdulilah <halkawt.abdulilah@gmail.com>
2024-01-14 14:36:32 +0400

    Auto version number generation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"expvar"
 	"flag"
+	"fmt"
 	"os"
 	"runtime"
 	"strings"
@@ -14,11 +15,14 @@ import (
 	"comicfleet.halkawtabdulilah.net/internal/data"
 	"comicfleet.halkawtabdulilah.net/internal/jsonlog"
 	"comicfleet.halkawtabdulilah.net/internal/mailer"
+	"comicfleet.halkawtabdulilah.net/internal/vcs"
 
 	_ "github.com/lib/pq"
 )
 
-const version = "1.0.0"
+var (
+	version = vcs.Version()
+)
 
 type config struct {
 	port int
@@ -86,7 +90,14 @@ func main() {
 		return nil
 	})
 
+	displayVersion := flag.Bool("version", false, "Display version and exit")
+
 	flag.Parse()
+
+	if *displayVersion {
+		fmt.Printf("Version:\t%s\n", version)
+		os.Exit(0)
+	}
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
